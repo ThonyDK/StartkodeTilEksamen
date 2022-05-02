ae45aca51d8b784dd5fcd462cf4d36ed6ca23dd2
Thony <thon0101@stud.kea.dk>
2022-05-02 11:06:33 +0200

    second commit

---
+++ src/test/java/com/example/startkodetileksamen/Repositories/TestRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.startkodetileksamen.Repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
---
+++ src/test/java/com/example/startkodetileksamen/Services/TestEntityServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.startkodetileksamen.Services;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestEntityServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
